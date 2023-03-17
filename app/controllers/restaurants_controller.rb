492da4a291082ac583fba0942802a3980963462d
Donte Ladatto <donte279@gmail.com>
2023-03-17 11:07:00 -0500

    restaurant actions and validations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    render json: Restaurant.all, status: :ok
+  end
+
+  def create
+    restaurant = Restaurant.create!(restaurant_params)
+    render json: restaurant, status: :created
+  end
+
+  private
+
+  def restaurant_params
+    params.permit(:name, :address)
+  end
+    
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
   has_many :meals, through: :reviews
+  validates_presence_of :name, :address
 end
