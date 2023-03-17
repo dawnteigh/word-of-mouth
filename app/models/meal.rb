b8b1b51ee4871e3fd45e8332179d37b4cefd4b9a
Donte Ladatto <donte279@gmail.com>
2023-03-17 12:15:50 -0500

    change meal name validation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,5 @@ class Meal < ApplicationRecord
   has_many :reviews
   has_many :restaurants, through: :reviews
   accepts_nested_attributes_for :reviews
-  validates_presence_of :name
+  validates :name, presence: true, uniqueness: true
 end
