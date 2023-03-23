c0292968b0e46ecf746fdc44b64ae8b1ba4f633e
Donte Ladatto <donte279@gmail.com>
2023-03-23 17:25:04 -0500

    more client side routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,10 @@ import Home from './Home'
 import NavBar from './NavBar'
 import Login from './Login'
 import Signup from './Signup'
+import ReviewNew from './ReviewNew'
+import UserProfile from './UserProfile'
+import Meals from './Meals';
+import Meal from './Meal'
 
 function App() {
   return (
@@ -15,7 +19,11 @@ function App() {
       <NavBar />
       <Home />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={ <Home />} />
+        <Route path="/review" element={ <ReviewNew /> } />
+        <Route path="/:username" element={ <UserProfile /> } />
+        <Route path="/meals" element={ <Meals /> } />
+        <Route path="/meals/:mealId" element={ <Meal /> } />
       </Routes>
     </div>
   );
