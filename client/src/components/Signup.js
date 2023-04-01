77b1774ab6f6dfa5a6a40fce02c24c0375d09e37
Donte Ladatto <donte279@gmail.com>
2023-03-31 19:56:13 -0500

    add signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+
+  const [signup, setSignup] = useState({
+    username: "",
+    password: "",
+    password_confirmation: ""
+  })
+
+  const handleChange = (e) => {
+    const value = e.target.value
+    setSignup({
+      ...signup,
+      [e.target.name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //fetch to /signup
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <form onSubmit={handleSubmit} >
+        <input
+          type="text"
+          label="Username" 
+          placeholder="Username"
+          name="username"
+          value={signup.username}
+          onChange={handleChange}
+          />
+        <input
+          type="password"
+          label="Password" 
+          placeholder="Password"
+          name="password"
+          value={signup.password}
+          onChange={handleChange}
+        />
+         <input
+          type="password"
+          label="Password" 
+          placeholder="Confirm Password"
+          name="password_confirmation"
+          value={signup.password_confirmation}
+          onChange={handleChange}
+        />
+        <input type="submit" />
+      </form>
+    </div>
   )
 }
 
