5d14d3c136a02841563e017269d0f06a99daa7aa
Donte Ladatto <donte279@gmail.com>
2023-03-31 20:15:09 -0500

    add NavLinks to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import SearchBar from './SearchBar'
 //will have search bar in addition to nav buttons
 const NavBar = () => {
+
   return (
     <div>
+      <nav id="navbar">
+        <NavLink
+          to="/"
+          className= {({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+        Home
+        </NavLink>
+        <NavLink
+          to="/meals"
+          className= {({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+        Meals
+        </NavLink>
+        <NavLink
+          to="/restaurants"
+          className= {({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+        Restaurants
+        </NavLink>
+        <NavLink
+          to="/review"
+          className= {({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+        Create Review
+        </NavLink>
+      </nav>
       <SearchBar />
     </div>
   )
