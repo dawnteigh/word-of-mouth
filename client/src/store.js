44281751f49a8d9d1d1b054e1c84118d0a421037
Donte Ladatto <donte279@gmail.com>
2023-04-05 13:25:26 -0500

    remove requestingReducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import sessionsReducer from "./reducers/sessionsSlice"
-import requestingReducer from "./reducers/requestingReducer"
-import errorsReducer from "./reducers/errorsReducer"
+import sessionsReducer from "./reducers/sessionsSlice"import errorsReducer from "./reducers/errorsReducer"
 import mealsReducer from "./reducers/mealsSlice"
 import restaurantsReducer from "./reducers/restaurantsSlice"
 
 const store = configureStore({
   reducer: {
     errors: errorsReducer,
-    requesting: requestingReducer,
     sessions: sessionsReducer,
     meals: mealsReducer,
     restaurants: restaurantsReducer
