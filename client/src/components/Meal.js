cddfbe774f51312865e7b0008d8a8a9e92307ab1
Donte Ladatto <donte279@gmail.com>
2023-04-16 11:56:15 -0500

    sort restaurants by best rating

--- client/src/components/Meal.js
+++ client/src/components/Meal.js
@@ -7,7 +7,25 @@ const Meal = () => {
   let { mealId } = useParams()
   const meal = useSelector(state => state.meals.entities.find(m => m.id === parseInt(mealId)))
 
-  const renderRestaurants = meal.restaurants.map(r => {
+  const restaurantsWithAvg = meal.restaurants.map(r => {
+    const review = meal.reviews.find(rev => rev.restaurant.id === r.id)
+    return {
+        ...r,
+        avg_rating: review.avg_rating
+    }  
+  })
+
+  const sortByRating = (a, b) => {
+    let rA = a.avg_rating
+    let rB = b.avg_rating
+    if(rA < rB) return 1;
+    if(rA > rB) return -1;
+    return 0;
+  }
+
+  const sortedRestaurants = restaurantsWithAvg.sort(sortByRating)
+
+  const renderRestaurants = sortedRestaurants.map(r => {
     return (
       <Restaurant key={r.id} restaurant={r} reviews={meal.reviews.filter(rev => rev.restaurant.id === r.id)} />
       )
